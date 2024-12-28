30e51a7325d1fffa734b3756a8999b6055bb4e3d
bnell267 <jessepereira170@gmail.com>
2024-12-28 15:12:34 -0300

    Update prelogin.php

--- prelogin.php
+++ prelogin.php
@@ -1,4 +1,19 @@
+<?php
+$dbHost = 'sql213.infinityfree.com';
+$dbUsername = 'if0_37950266';
+$dbPassword = 'ajpda170a';
+$dbName = 'if0_37950266_teste';
 
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+if($conexao->connect_errno){
+echo "erro";}
+
+else
+{
+echo "STATUS:conectado";
+}
+?>
 <?php
  //includ('config.php');
 

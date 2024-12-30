5addf1cb77dec03172128966220ec77720ac857b
bnell267 <jessepereira170@gmail.com>
2024-12-30 13:05:44 -0300

    Update prelogin.php

--- prelogin.php
+++ prelogin.php
@@ -1,28 +1,7 @@
 
 <?php
- include('config.php');
-//php(info);
-
-iff(empty($_POST['senha'])){header('Location:vazionot.php');}
-$senha=$_POST['senha'];
-$chave=$_POST['chave'];
-$res=mysqli_query($conexao,"SELECT * FROM testenv where senha='$senha' AND chave='$chave'");
-
-
-iff(mysqli_num_rows($res)>=1){
-
-print_r($_REQUEST);
-SESSION_START();
-
-$_SESSION['senha']=$senha;
-$_SESSION['id']=$id;
-
-header('Location:pag ini.php');}
-}
-
-{
-
- header('Location:unautorized.php');}
+ include 'config.php';
+echo "teste";
   
   
   

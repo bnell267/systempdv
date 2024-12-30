1fd8ee103568c940c38cfed4a70f4cdb8b52c7cc
bnell267 <jessepereira170@gmail.com>
2024-12-30 08:41:19 -0300

    Update prelogin.php

--- prelogin.php
+++ prelogin.php
@@ -1,24 +1,24 @@
 
 <?php
- //include('config.php');
+ include('config.php');
 //php(info);
 
-if(empty($_POST['senha'])){header('Location:vazionot.php');}
+iff(empty($_POST['senha'])){header('Location:vazionot.php');}
 $senha=$_POST['senha'];
 $chave=$_POST['chave'];
 $res=mysqli_query($conexao,"SELECT * FROM testenv where senha='$senha' AND chave='$chave'");
 
 
-if(mysqli_num_rows($res)>=1){
+iff(mysqli_num_rows($res)>=1){
 
-echo($_REQUEST);
+print_r($_REQUEST);
 SESSION_START();
 
 $_SESSION['senha']=$senha;
 $_SESSION['id']=$id;
 
 header('Location:pag ini.php');}
-}else
+}
 
 {
 

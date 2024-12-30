5604b9896b394b9e4dac322a3de275bf8b39f4d3
bnell267 <jessepereira170@gmail.com>
2024-12-30 08:39:08 -0300

    Update prelogin.php

--- prelogin.php
+++ prelogin.php
@@ -3,7 +3,7 @@
  //include('config.php');
 //php(info);
 
-//if(empty($_POST['senha'])){header('Location:vazionot.php');}
+if(empty($_POST['senha'])){header('Location:vazionot.php');}
 $senha=$_POST['senha'];
 $chave=$_POST['chave'];
 $res=mysqli_query($conexao,"SELECT * FROM testenv where senha='$senha' AND chave='$chave'");
@@ -12,17 +12,17 @@ $res=mysqli_query($conexao,"SELECT * FROM testenv where senha='$senha' AND chave
 if(mysqli_num_rows($res)>=1){
 
 echo($_REQUEST);
-//SESSION_START();
+SESSION_START();
 
-//$_SESSION['senha']=$senha;
-//$_SESSION['id']=$id;
+$_SESSION['senha']=$senha;
+$_SESSION['id']=$id;
 
-//header('Location:pag ini.php');}
+header('Location:pag ini.php');}
 }else
 
 {
 
- // header('Location:unautorized.php');}
+ header('Location:unautorized.php');}
   
   
   

fa1872367bcbefc6043713b0f82c453f79573479
bnell267 <jessepereira170@gmail.com>
2024-12-29 21:25:45 -0300

    Update precad.php

--- precad.php
+++ precad.php
@@ -1,6 +1,6 @@
 <?php 
 echo '<form action= "preprecad.php" method="post"name="cad">';
-echo'<input type="text"name="chave">';
-echo'<input type="submit"value="ENTRAR"/>';
-echo '<form/>;
+echo' <input type="text"name="chave">';
+echo' <input type="submit"value="ENTRAR"/>';
+echo '<form/>';
 ?>
